36600ee206d249337fbfece5fa42655b726fbd39
xuyonghui <564146958@qq.com>
2019-10-21 21:56:26 +0800

    上传图片功能封装成功,icon补充

--- pages/add-input/add-input.vue
+++ pages/add-input/add-input.vue
@@ -3,32 +3,45 @@
 		<view class="example-body">
 			<uni-nav-bar :statusBar="true" left-icon="arrowleft" rightText="发布" @click-left="back" @click-right="submit">
 			<view class="uni-flex u-text" @click="change">
-				{{text}}<view class=""></view>
+				{{text}}<view class="iconfont icon-down" style="margin-left: 10upx;vertical-align: middle;"></view>
 			</view>
 			</uni-nav-bar>
 		</view>
+		<!-- 文本域 -->
+		<view class="uni-textarea">
+			<textarea v-model="textarea" placeholder="说点什么把" />
+		</view>
+		<!-- 上传多图 -->
+		<uploadImages @upload="uploads"></uploadImages>
+		
 	</view>
 </template>
 
 <script>
+	
 	import uniNavBar from '../../compument/uni-nav-bar/uni-nav-bar.vue'
+	import uploadImages from '../../compument/upload-images/lupload-images.vue'
 	export default {
 		data() {
 			return {
-				text:'所有人可见'
+				text:'所有人可见',
+				textarea:'',
+				listimages:[]
 			}
 		},
 		components:{
-			uniNavBar
+			uniNavBar,
+			uploadImages
 		},
 		methods: {
+			uploads(arr){
+				this.listimages = arr;
+				console.log(this.listimages)
+			},
 			back(){
 				uni.navigateBack({
 					delta:1
 				})
-			},
-			submit(){
-				console.log('发布')
 			},
 			change(){
 				uni.showActionSheet({
@@ -39,18 +52,23 @@
 							this.text="所有人可见";
 						}else{
 							this.text = "仅自己可见";
-							
 						}
 					}
 				})
 			}
+			
 		}
 	}
 </script>
 
 <style scoped>
-.u-text{
-	display: flex;
-	margin: 0 auto;
-}
+	.uni-navbar__content{
+		box-shadow: none;
+	}
+	.u-text{
+		display: flex;
+		margin: 0 auto;
+	}
+
+		
 </style>
