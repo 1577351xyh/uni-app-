00cc22f9a3fe8dc50de07a07c4fa6739a80821e4
xuyonghui <564146958@qq.com>
2019-10-30 23:50:13 +0800

    详情页头部配置和样式完成

---
+++ pages/topic-detail/topic-detail.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+		<topic-iofo :item="topicInfo"></topic-iofo>
+	</view>
+</template>
+
+<script>
+	import topicIofo from '../../compument/topic/topic-info.vue'
+	export default {
+		data() {
+			return {
+				topicInfo:{
+					titlepic:'../../static/images/userpic/8.jpg',
+					desc:'我是描述',
+					title:'我是标题',
+					totalnum:20,
+					totaynum:20
+				}
+			}
+		},
+		components:{
+			topicIofo
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
